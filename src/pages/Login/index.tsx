04a72fe2896f3872fcc3e90237d58cb9c70ba868
Fahad Ahmed <fahad.ahmed@me.com>
2021-05-19 13:15:00 +1000

    TSTTY-1 implemented firebase auth

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { Link, Redirect, withRouter } from 'react-router-dom';
+import {AppContext} from '../../components/AppProvider/AppContext';
+import { auth } from '../../config/firebase';
 
-function Login(): JSX.Element {
+function Login({history}): JSX.Element {
+  const [error, setError] = useState(null);
+  
+  const handleLogin = event => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    const lgn = auth().signInWithEmailAndPassword(email.value, password.value)
+    .catch(err => {
+      console.log(err);
+      setError(err.message);
+    })
+  };
+
+  const { currentUser } = useContext(AppContext);
+  if(currentUser) {
+    return <Redirect to='/dashboard' />
+  }
+
   return(
     <>
       <h1>Login Page</h1>
       <div>
         <Link to='/home'>Home Page</Link>
+        <form onSubmit={handleLogin}>
+          <input type="email" placeholder="email address" name="email" required />
+          <input type="password" placeholder="password" name="password" required />
+          <button type="submit">Login</button>
+          {error !== null && <p>Error: {error}</p>}
+        </form>
       </div>
     </>
   )
