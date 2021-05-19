88be9e91a58ffc53fa73100302577b814378d5eb
Fahad Ahmed <fahad.ahmed@me.com>
2021-05-19 15:06:39 +1000

    TSTTY-1 updated Dashboard component to show timer and logged in user's name

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { auth } from '../../config/firebase';
+import { AppContext } from '../../components/AppProvider/AppContext';
+import Timer from '../../components/Timer';
 
 function Dashboard(): JSX.Element {
+  const { currentUser } = useContext(AppContext);
+  console.log(currentUser);
   return(
     <div>
       <h1>Dashboard Page</h1>
       <button onClick={() => auth().signOut()}>Log out</button>
+      <h2>{currentUser.displayName}</h2>
+      <Timer />
     </div>
   )
 }
