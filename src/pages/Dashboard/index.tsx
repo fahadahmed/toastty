f7ffc100a2860209f71fa7f75c193796094220a0
Fahad Ahmed <fahad.ahmed@me.com>
2021-05-23 11:59:27 +1000

    TSTTY-1 cleanup of the dashboard page code

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,30 +1,9 @@
-import React, { useState, useContext } from 'react';
-import { AppContext } from '../../components/AppProvider/AppContext';
-import Timer from '../../components/Timer';
+import React from 'react';
 import Header from '../../components/Header';
 import AddEntry from './components/AddEntry';
 import Entries from './components/Entries';
 
 function Dashboard(): JSX.Element {
-  const { currentUser } = useContext(AppContext);
-  const [entries, setEntries] = useState([]);
-  
-  const handleNewEntry = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & {
-      project: { value: string };
-      client: { value: string };
-    };
-
-    const project = target.project.value;
-    const client = target.client.value;
-
-    const entry = { project, client, user: currentUser.displayName };
-    setEntries(prevState => ([
-      ...prevState,
-      entry
-    ]));
-  }
   return(
     <div>
       <Header />
