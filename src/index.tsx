b9c6f4702ccdf4c9edad56ae2f9e046a060baf60
Fahad Ahmed <fahad.ahmed@me.com>
2021-05-29 16:20:04 +1000

    TSTTY-11 project reset to accomodate nx workspaces

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './pages/routes';
-import "core-js/stable";
-import "regenerator-runtime/runtime";
-
-ReactDOM.render(<Routes />, document.querySelector('#root'));
