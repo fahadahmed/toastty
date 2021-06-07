5ad687b70b19dc0d58e661d3035ba150ed51d9b7
Fahad Ahmed <fahad.ahmed@me.com>
2021-06-07 22:58:39 +1000

    TSTTY-8 made the addEntry component responsive

--- src/pages/Dashboard/components/AddEntry/styles.ts
+++ src/pages/Dashboard/components/AddEntry/styles.ts
@@ -3,6 +3,10 @@ import { fontColor, borderColor } from '../../../../styles/variables';
 
 const Container = styled.div`
   padding: 40px 80px;
+
+  @media screen and (max-width: 425px) {
+    padding: 10px;
+  }
 `;
 const Form = styled.form`
   border: 1px solid ${borderColor};
@@ -12,6 +16,10 @@ const Form = styled.form`
   display: grid;
   grid-template-columns: 10fr 2fr;
   grid-gap: 30px;
+
+  @media screen and (max-width: 425px) {
+    grid-template-columns: 1fr;
+  }
 `;
 const TimerContainer = styled.div`
   display: grid;
