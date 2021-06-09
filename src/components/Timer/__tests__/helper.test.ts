fb59b212dac15859cc16afcc83c4a1a5a9ce74f9
Fahad Ahmed <fahad.ahmed@me.com>
2021-06-09 22:50:40 +1000

    TSTTY-7 updated code to collect code coverage

--- src/components/Timer/__tests__/helper.test.ts
+++ src/components/Timer/__tests__/helper.test.ts
@@ -2,5 +2,7 @@ import {formatTime } from '../helper';
 describe("unit: helper.ts file", () => {
   it("should return a formated value", () => {
     expect(formatTime(0)).toBe('00 : 00');
+    expect(formatTime(60)).toBe('01 : 00');
+    expect(formatTime(3600)).toBe('01 : 00 : 00');
   });
 });
