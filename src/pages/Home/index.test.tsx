0f741e8dbb33d3f0136729dc3953c159f39aa523
Fahad Ahmed <fahad.ahmed@me.com>
2021-06-13 13:32:15 +1000

    TSTTY-7 minor updates to unit test code

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -3,15 +3,18 @@ import Home from './index';
 import { render, getByTestId, getByText } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { AppProvider } from '../../components/AppProvider/AppContext';
+import AppContainer from '../../components/AppContainer';
 
 describe('unit: Home Page', () => {
   it('renders properly', () => {
     const currentUser = jest.fn();
     const { getByText } = render(
       <AppProvider>
-        <MemoryRouter>
-          <Home />
-        </MemoryRouter>
+        <AppContainer>
+          <MemoryRouter>
+            <Home />
+          </MemoryRouter>
+        </AppContainer>
       </AppProvider>
     );
 
